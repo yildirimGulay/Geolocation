5bd83a13eae15b9f17aa67209149c57328a8d9ca
yildirimGulay <gulaycontact@gmail.com>
2021-03-14 00:18:24 +0000

    Geolacation Api is implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = ()=> {
+  navigator.geolocation.getCurrentPosition(
+    (konum)=>console.log(`konum:`, konum.coords.latitude),
+    (hata)=>console.log(`hata: `, hata.message)
+  );
+
+  return(
+
+<div>
+  <h1> This season app</h1>
+</div>
+
+  );
+
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
